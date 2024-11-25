851048579d1bd640e71d64964079c2980870e859
Jefferson <jeff.dev.simplicio@gmail.com>
2024-11-25 17:22:46 -0300

    Adicionanddo funcção de pesquisar jogos

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import time
+
 import pyautogui as py
 import random
 
@@ -6,6 +8,16 @@ JOGOS_CADASTRADOS = []
 
 def clear():
     py.hotkey('ctrl', ',')
+
+
+def show_jogo(jogo):
+    print(f"""
+    Id: {jogo['id']}
+    Nome: {jogo['nome']}
+    Criador: {jogo['criador']}
+    Distribuidora: {jogo['distribuidora']}
+    Ano de lançamento: {jogo['ano_lancamento']} 
+    """)
 
 
 def adicionar_jogo():
@@ -30,37 +42,57 @@ def adicionar_jogo():
     if ver_jogo not in nao and ver_jogo not in sim:
         print('Opção inválida, seguindo processo.')
     elif ver_jogo in sim:
-        print(jogo)
+        show_jogo(jogo)
+        time.sleep(5)
+        clear()
     elif ver_jogo in nao:
         pass
+
+
+def pesquisar_jogo():
+    clear()
+    to_search = str(input('Pesquise pelo nome ou id de um jogo: ')).lower()
+    find_game = None
+    for jogo in JOGOS_CADASTRADOS:
+        if jogo['id'] == to_search or jogo['nome'] == to_search:
+            find_game = jogo
+    if find_game:
+        show_jogo(find_game)
+    else:
+        print('Jogo não encontrado!')
+
+
+def listar_jogos():
+    pass
 
 
 def menu():
     opcoes_validas = [1, 2, 3, 4]
     empty_line = "*************************************************"
-    print(f"""
-    {empty_line}
-    *******BEM VINDO A SUA BIBLIOTECA DE JOGOS*******
-    {empty_line}
-    * 1. Adicionar um jogo                          *
-    * 2. Pesquisar por um jogo                      *
-    * 3. Listar todos os jogos                      *
-    * 4. Sair                                       *
-    {empty_line}
-    {empty_line}
-    {empty_line}
+    while True:
+        print(f"""
+{empty_line}
+*******BEM VINDO A SUA BIBLIOTECA DE JOGOS*******
+{empty_line}
+* 1. Adicionar um jogo                          *
+* 2. Pesquisar por um jogo                      *
+* 3. Listar todos os jogos                      *
+* 4. Sair                                       *
+{empty_line}
+{empty_line}
+{empty_line}
     """)
-    opcao = int(input("Informe a opção desejada "))
-    if opcao not in opcoes_validas:
-        print("Opção inválida. ")
-    if opcao == 1:
-        adicionar_jogo()
-    elif opcao == 2:
-        pass
-    elif opcao == 3:
-        pass
-    elif opcao == 4:
-        pass
+        opcao = int(input("Informe a opção desejada "))
+        if opcao not in opcoes_validas:
+            print("Opção inválida. ")
+        if opcao == 1:
+            adicionar_jogo()
+        elif opcao == 2:
+            pesquisar_jogo()
+        elif opcao == 3:
+            listar_jogos()
+        elif opcao == 4:
+            break
 
 
 def main():
